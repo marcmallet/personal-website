f7be0336e8933399bdba60749303e6c036195060
Marc Mallet <marc@insynnc.com>
2023-06-28 10:16:38 -0400

    Update text

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -10,10 +10,10 @@ function Hero() {
           <h2>UI Designer <span>&bull;</span> Developer</h2>
         </hgroup>
         <p>
-          I&apos;m a UI Designer and Full-stack Developer with a profound love for crafting immersive digital
-          experiences.
-          With a unique blend of creativity and technical expertise, I strive to deliver user-centric designs that
-          not only captivate users but also align with the business goals.
+          I&apos;m a UI Designer and Full-stack Developer and love for crafting visually appealing and fluent digital
+          experiences. I have been writing code for over 15 years and love learning new things to continuously grow my skills.
+          I have a unique blend of creativity and technical expertise and focus on delivering user-centric
+          designs and features that align with business goals.
         </p>
         <ul>
           <li><a href="https://www.linkedin.com/in/marc-mallet-a1059815/" target="_blank" rel="noopener noreferrer" className={styles.linkedin}></a></li>
