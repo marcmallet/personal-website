568b4e2efb2c2265c01e448f9777ca1ba491e4cb
Marc Mallet <marc@insynnc.com>
2023-07-01 16:50:49 -0400

    Fix broken links on social networks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <li><a href="https://www.linkedin.com/in/marc-mallet-a1059815/" target="_blank" rel="noopener noreferrer" className={styles.linkedin}></a></li>
         <li><a href="https://dribbble.com/marcmallet/" target="_blank" rel="noopener noreferrer" className={styles.dribbble}></a></li>
         <li><a href="https://www.instagram.com/marcmallet.ui/" target="_blank" rel="noopener noreferrer" className={styles.instagram}></a></li>
-        <li><a href="https://https://github.com/marcmallet/" target="_blank" rel="noopener noreferrer" className={styles.github}></a></li>
+        <li><a href="https://github.com/marcmallet/" target="_blank" rel="noopener noreferrer" className={styles.github}></a></li>
       </ul>
     </footer>
   )
