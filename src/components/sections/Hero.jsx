d3a35449533211af16d1ce9392b5f1b8ad5e85b0
Marc Mallet <marc@insynnc.com>
2023-07-04 09:40:42 -0400

    Remove instagram links

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -18,7 +18,6 @@ export default function Hero({ id }) {
           <ul>
             <li><a href="https://www.linkedin.com/in/marc-mallet-a1059815/" target="_blank" rel="noopener noreferrer" className={styles.linkedin}></a></li>
             <li><a href="https://dribbble.com/marcmallet/" target="_blank" rel="noopener noreferrer" className={styles.dribbble}></a></li>
-            <li><a href="https://www.instagram.com/marcmallet.ui/" target="_blank" rel="noopener noreferrer" className={styles.instagram}></a></li>
             <li><a href="https://github.com/marcmallet/" target="_blank" rel="noopener noreferrer" className={styles.github}></a></li>
           </ul>
         <div className={styles.mouse}></div>
